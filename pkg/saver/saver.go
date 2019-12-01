85bc4ddd11b57c73bf34f0833c4e78bffc7aa9a1
Jamie <ziadoz@gmail.com>
2019-12-01 23:11:05 +0000

    Test saving media. Minor refactor of methods.

--- pkg/saver/saver.go
+++ pkg/saver/saver.go
@@ -17,30 +17,48 @@ type TweetSaver struct {
 }
 
 func (ts *TweetSaver) Save(tweet twitter.Tweet) error {
-	tweetId := strconv.FormatInt(tweet.ID, 10)
-
 	if ts.SaveJson {
-		bytes, err := json.MarshalIndent(tweet, "", "    ")
-		if err != nil {
-			return fmt.Errorf("could not marshal tweet JSON: %s", err)
-		}
-
-		if err := ioutil.WriteFile(path.Join(ts.SaveDir, tweetId+".json"), bytes, 0644); err != nil {
-			return fmt.Errorf("could not write JSON file: %s", err)
+		if err := ts.saveJson(tweet); err != nil {
+			return err
 		}
 	}
 
 	if ts.SaveMedia && hasMedia(tweet) {
-		num := 1
-		for _, media := range extractMedia(tweet.ExtendedEntities.Media) {
-			ext, err := getExtensionFromURL(media)
-			if err != nil {
-				return fmt.Errorf("could not save tweet ID %s media: %s", tweetId, media)
-			}
-
-			saveMediaFromURL(media, path.Join(ts.SaveDir, tweetId+"-"+strconv.Itoa(num)+ext))
-			num++
+		if err := ts.saveMedia(tweet); err != nil {
+			return err
 		}
+	}
+
+	return nil
+}
+
+func (ts *TweetSaver) saveJson(tweet twitter.Tweet) error {
+	tweetId := strconv.FormatInt(tweet.ID, 10)
+
+	bytes, err := json.MarshalIndent(tweet, "", "    ")
+	if err != nil {
+		return fmt.Errorf("could not marshal tweet JSON: %s", err)
+	}
+
+	if err := ioutil.WriteFile(path.Join(ts.SaveDir, tweetId+".json"), bytes, 0644); err != nil {
+		return fmt.Errorf("could not write JSON file: %s", err)
+	}
+
+	return nil
+}
+
+func (ts *TweetSaver) saveMedia(tweet twitter.Tweet) error {
+	tweetId := strconv.FormatInt(tweet.ID, 10)
+
+	num := 1
+	for _, media := range extractMedia(tweet.ExtendedEntities.Media) {
+		ext, err := getExtensionFromURL(media)
+		if err != nil {
+			return fmt.Errorf("could not save tweet ID %s media: %s", tweetId, media)
+		}
+
+		saveMediaFromURL(media, path.Join(ts.SaveDir, tweetId+"-"+strconv.Itoa(num)+ext))
+		num++
 	}
 
 	return nil
--- pkg/saver/saver_test.go
+++ pkg/saver/saver_test.go
@@ -3,6 +3,8 @@ package saver
 import (
 	"bytes"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
@@ -36,4 +38,43 @@ func TestTweetSaverSaveJson(t *testing.T) {
 	fbytes, _ := ioutil.ReadFile("./fixtures/1234567890.json")
 	obytes, _ := ioutil.ReadFile("./output/1234567890.json")
 	is.True(bytes.Equal(fbytes, obytes))
+}
+
+func TestTweetSaverSaveMedia(t *testing.T) {
+	is := is.New(t)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./fixtures/omg.gif")
+	}))
+	defer ts.Close()
+
+	url := ts.URL + "/omg.gif" // Add filename to URL.
+	tweet := twitter.Tweet{
+		ID:        tweetId,
+		CreatedAt: "2019-01-01 01:01:01",
+		Text:      "This is a test tweet. @helloworld http://www.example.com",
+		ExtendedEntities: &twitter.ExtendedEntity{
+			Media: []twitter.MediaEntity{
+				twitter.MediaEntity{
+					MediaURLHttps: url,
+					Type:          "animated_gif",
+					VideoInfo: twitter.VideoInfo{
+						Variants: []twitter.VideoVariant{
+							twitter.VideoVariant{
+								URL: url,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	saver := TweetSaver{
+		SaveDir:   "./output",
+		SaveMedia: true,
+	}
+
+	err := saver.Save(tweet)
+	is.NoErr(err)
 }
